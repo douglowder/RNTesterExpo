a0dccf6733853bef9a7316f1fc1549e598c41ce3
Douglas Lowder <douglowder@mac.com>
2024-08-26 01:10:53 -0700

    Move to RNTV 0.75.2-0rc0 prerelease

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DEBUG=expo:* HERMES_ARTIFACT_FROM_MAVEN_LOCAL=1 EXPO_TV=1 npx expo prebuild --clean
+# Normal TV prebuild
+#
+DEBUG=expo:* EXPO_TV=1 npx expo prebuild --clean
 
-echo 'react.internal.mavenLocalRepo=/tmp/maven-local' >> android/gradle.properties
+# Use the lines below if building using local Maven artifacts
+#
+# DEBUG=expo:* HERMES_ARTIFACT_FROM_MAVEN_LOCAL=1 EXPO_TV=1 npx expo prebuild --clean
+# echo 'react.internal.mavenLocalRepo=/tmp/maven-local' >> android/gradle.properties
+
