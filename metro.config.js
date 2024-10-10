c030d586b8c6d6045f027f061c9e59f07a20d938
Douglas Lowder <douglowder@mac.com>
2024-10-10 01:48:28 -0700

    Metro changes:
    - Use Hermes parser for 0.76

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname); // eslint-disable-line no-undef
+
+// Add Hermes parser
+config.transformer.hermesParser = true;
 
 // When enabled, the optional code below will allow Metro to resolve
 // and bundle source files with TV-specific extensions
@@ -10,7 +14,6 @@ const config = getDefaultConfig(__dirname);
 // Metro will still resolve source files with standard extensions
 // as usual if TV-specific files are not found for a module.
 //
-/*
 if (process.env?.EXPO_TV === '1') {
   const originalSourceExts = config.resolver.sourceExts;
   const tvSourceExts = [
@@ -19,6 +22,5 @@ if (process.env?.EXPO_TV === '1') {
   ];
   config.resolver.sourceExts = tvSourceExts;
 }
- */
 
 module.exports = config;
